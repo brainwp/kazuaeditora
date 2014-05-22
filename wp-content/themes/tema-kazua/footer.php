6051257182278f3b3815b669663bfd0b8f1925fc
Diego Rojas <diego@brasa.art.br>
2014-05-22 05:59:53 -0300

    tema child estruturado, wordpress atualizado

---
+++ wp-content/themes/tema-kazua/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+				<?php do_action( 'twentytwelve_credits' ); ?>		
+				<a href="<?php echo esc_url( __( 'http://editorakazua.com.br', 'Editora Kazuá Ltda - 2013' ) ); ?>" title="<?php esc_attr_e( 'ir para home page', 'twentyeleven' ); ?>"><?php printf( __( 'Editora Kazuá Ltda - 2013 <br /> CNPJ 16.569.856/0001-12 ' ), 'Editora Kazuá' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
