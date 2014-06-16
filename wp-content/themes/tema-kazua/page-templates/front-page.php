c6c16ff4202a809924990f877b0445c0f3b9c7aa
Diego Rojas <diego@brasa.art.br>
2014-06-16 07:25:02 -0300

    adicionado target _blank nos itens da loja da home e retirado entry-meta do content principal

--- wp-content/themes/tema-kazua/page-templates/front-page.php
+++ wp-content/themes/tema-kazua/page-templates/front-page.php
@@ -55,79 +55,79 @@ get_header(); ?>
 				</a><!-- Fim logo-loja -->
 				<div id="catalogo">
 					<div class="exemplar" >
-					<a href="http://www.zungueira.com.br/lancamentos/eu-sou-o-que-rezo-pra-mim-de-negra-anastacia.html">
+					<a href="http://www.zungueira.com.br/lancamentos/eu-sou-o-que-rezo-pra-mim-de-negra-anastacia.html" target="_blank">
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/eu_sou_o_que_rezo_pra_mim-capa.jpg" width="100%" height="auto" alt="" />
 						</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/eu-sou-o-que-rezo-pra-mim-de-negra-anastacia.html">Eu sou o que rezo pra mim, de Negra Anastácia</a>
+						<a class="title" href="http://www.zungueira.com.br/lancamentos/eu-sou-o-que-rezo-pra-mim-de-negra-anastacia.html" target="_blank">Eu sou o que rezo pra mim, de Negra Anastácia</a>
 					</div>
 					<div class="exemplar" >
-						<a href="http://www.zungueira.com.br/lancamentos/poema-preta-de-negra-anastacia.html">
+						<a href="http://www.zungueira.com.br/lancamentos/poema-preta-de-negra-anastacia.html" target="_blank">
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/poema-preta-capa.jpg"  width="100%" height="auto" alt=""/>
 					</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/poema-preta-de-negra-anastacia.html">Poema Preta, de Negra Anastácia</a>
+						<a class="title" href="http://www.zungueira.com.br/lancamentos/poema-preta-de-negra-anastacia.html" target="_blank">Poema Preta, de Negra Anastácia</a>
 					</div>					
 					<div class="exemplar" >
-						<a href="http://www.zungueira.com.br/lancamentos/o-mar-e-quando-mergulho-de-negra-anastacia.html">
+						<a href="http://www.zungueira.com.br/lancamentos/o-mar-e-quando-mergulho-de-negra-anastacia.html" target="_blank">
 							<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/o-mar-quando-mergulho-capa.jpg" width="100%" height="auto" alt="" />
 						</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/o-mar-e-quando-mergulho-de-negra-anastacia.html">O mar é quando mergulho, de Negra Anastácia</a>
+						<a class="title" href="http://www.zungueira.com.br/lancamentos/o-mar-e-quando-mergulho-de-negra-anastacia.html" target="_blank">O mar é quando mergulho, de Negra Anastácia</a>
 					</div>
 					<!--<div style="border:1px solid #777; width:100%; float:left"></div>-->
 					<div class="exemplar" >
-					<a href="http://www.zungueira.com.br/lancamentos/um-travesseiro-para-dois-marcio-rodrigues.html">	
+					<a href="http://www.zungueira.com.br/lancamentos/um-travesseiro-para-dois-marcio-rodrigues.html" target="_blank">	
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/um-travesseiro-para-dois-capa.jpg" width="100%" height="auto" alt="" />
 					</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/um-travesseiro-para-dois-marcio-rodrigues.html">Um travesseiro para dois, de Márcio Rodrigues</a>
+						<a class="title" href="http://www.zungueira.com.br/lancamentos/um-travesseiro-para-dois-marcio-rodrigues.html" target="_blank">Um travesseiro para dois, de Márcio Rodrigues</a>
 					</div>
 
 
 					<div class="exemplar" >
-					<a href="http://www.zungueira.com.br/lancamentos/o-ultimo-samba-enredo-paulo-fabiao.html">
+					<a href="http://www.zungueira.com.br/lancamentos/o-ultimo-samba-enredo-paulo-fabiao.html" target="_blank">
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/ultimo-samba-enredo-capa.jpg" width="100%" height="auto" alt="" />
 					</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/o-ultimo-samba-enredo-paulo-fabiao.html">O último samba-enredo, de Paulo Fabião</a>
+						<a class="title" href="http://www.zungueira.com.br/lancamentos/o-ultimo-samba-enredo-paulo-fabiao.html" target="_blank">O último samba-enredo, de Paulo Fabião</a>
 					</div>
 					<div class="exemplar" >
-					<a href="http://www.zungueira.com.br/lancamentos/entre-semioticas.html">
+					<a href="http://www.zungueira.com.br/lancamentos/entre-semioticas.html" target="_blank">
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/entre-semioticas-capa.jpg" width="100%" height="auto" alt="" />
 					</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/entre-semioticas.html">Entre-semióticas</a>
+						<a class="title" href="http://www.zungueira.com.br/lancamentos/entre-semioticas.html" target="_blank">Entre-semióticas</a>
 					</div>					
 					<div class="exemplar" >
-					<a href="http://www.zungueira.com.br/lancamentos/para-quando-o-entulho-soterrar-os-joelhos.html">
+					<a href="http://www.zungueira.com.br/lancamentos/para-quando-o-entulho-soterrar-os-joelhos.html" target="_blank">
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/para-quando-o-entulho-soterrar-os-joelhos-capa.jpg" width="100%" height="auto" alt=""  />
 					</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/para-quando-o-entulho-soterrar-os-joelhos.html">Para Quando o Entulho Soterrar os Joelhos</a>
+						<a class="title" href="http://www.zungueira.com.br/lancamentos/para-quando-o-entulho-soterrar-os-joelhos.html" target="_blank">Para Quando o Entulho Soterrar os Joelhos</a>
 					</div>
 					<div class="exemplar" >
-					<a href="http://www.zungueira.com.br/lancamentos/o-caos-do-acaso-de-mauro-nunes.html">
+					<a href="http://www.zungueira.com.br/lancamentos/o-caos-do-acaso-de-mauro-nunes.html" target="_blank">
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/o-caos-do-acaso-capa.jpg" width="100%" height="auto" alt="" />
 					</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/o-caos-do-acaso-de-mauro-nunes.html">O Caos do Acaso, de Mauro Nunes</a>
+					<a class="title" href="http://www.zungueira.com.br/lancamentos/o-caos-do-acaso-de-mauro-nunes.html" target="_blank">O Caos do Acaso, de Mauro Nunes</a>
 					</div>
 					<div class="exemplar" >
-						<a href="http://www.zungueira.com.br/lancamentos/memorias-heliopolis-criacao-coletiva.html">				
+						<a href="http://www.zungueira.com.br/lancamentos/memorias-heliopolis-criacao-coletiva.html" target="_blank">				
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/memorias-heliopolis-capa.jpg" width="100%" height="auto" alt="" />
 						</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/memorias-heliopolis-criacao-coletiva.html">Memórias de Heliópolis</a>
+						<a class="title" href="http://www.zungueira.com.br/lancamentos/memorias-heliopolis-criacao-coletiva.html" target="_blank">Memórias de Heliópolis</a>
 					</div>
 					<div class="exemplar" >
-					<a href="http://www.zungueira.com.br/lancamentos/meia-pata-ricardo-dantas.html">
+					<a href="http://www.zungueira.com.br/lancamentos/meia-pata-ricardo-dantas.html" target="_blank">
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/meia-pata-capa.jpg" width="100%" height="auto" alt="" />
 					</a>
-					<a class="title" href="http://www.zungueira.com.br/lancamentos/meia-pata-ricardo-dantas.html">Meia Pata, de Ricardo Dantas</a>
+					<a class="title" href="http://www.zungueira.com.br/lancamentos/meia-pata-ricardo-dantas.html" target="_blank">Meia Pata, de Ricardo Dantas</a>
 					</div>
 					<div class="exemplar" >
-					<a href="http://www.zungueira.com.br/lancamentos/dionisio-kore-marcelo-marcus-fonseca.html">
+					<a href="http://www.zungueira.com.br/lancamentos/dionisio-kore-marcelo-marcus-fonseca.html" target="_blank">
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/dionisio-kore-capa.jpg" width="100%" height="auto" alt="" />
 					</a>
-						<a class="title" href="http://www.zungueira.com.br/lancamentos/dionisio-kore-marcelo-marcus-fonseca.html">De Dionísio para Koré, de Marcelo Marcus Fonseca</a>
+						<a class="title" href="http://www.zungueira.com.br/lancamentos/dionisio-kore-marcelo-marcus-fonseca.html" target="_blank">De Dionísio para Koré, de Marcelo Marcus Fonseca</a>
 					</div>
 					<div class="exemplar" >
-					<a href="http://www.zungueira.com.br/no-diva-palhaco-carlos-biaggioli.html">
+					<a href="http://www.zungueira.com.br/no-diva-palhaco-carlos-biaggioli.html" target="_blank">
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/no-diva-do-palhaco-capa.jpg" width="100%" height="auto" alt="" />
 					</a>
-						<a class="title" href="http://www.zungueira.com.br/no-diva-palhaco-carlos-biaggioli.html">No divã do palhaço, de Carlos Biaggioli</a>
+						<a class="title" href="http://www.zungueira.com.br/no-diva-palhaco-carlos-biaggioli.html" target="_blank">No divã do palhaço, de Carlos Biaggioli</a>
 					</div>
 				</div>	
 				<a id="botao-de-baixo-loja" href="http://www.zungueira.com.br/" target="_blank">
