ed7466f02c85d1efc069691210d41732a9bacf75
matheus <contato@matheusgimenez.com.br>
2014-08-08 16:52:57 -0300

    mais um bug

--- wp-content/themes/tema-kazua/js/ajax-menu-loader.js
+++ wp-content/themes/tema-kazua/js/ajax-menu-loader.js
@@ -4,9 +4,9 @@
 (function ($) {
     'use strict';
     $('.nav-menu li a').on('click',function(e){
-        e.preventDefault();
         var _attr = $(this).attr('data-menu-ajax');
         if($('#ajax-menu-load-div').attr('data-ajax') == 'true' && _attr == 'true'){
+            e.preventDefault();
             var _href = $(this).attr('href') + '?AjaxMenuLoader=true';
             $('#ajax-menu-load-div').load(_href);
         }
