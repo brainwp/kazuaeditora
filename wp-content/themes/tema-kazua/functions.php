7bd26985271070119e09ccd84ab1e3940d2cfe85
matheus <contato@matheusgimenez.com.br>
2014-08-26 12:23:09 -0300

    jetpack galeria funcionando

--- wp-content/themes/tema-kazua/functions.php
+++ wp-content/themes/tema-kazua/functions.php
@@ -17,14 +17,75 @@ function add_themescript()
         wp_enqueue_script('jquery');
         wp_enqueue_script('thickbox', null, array('jquery'));
         wp_enqueue_style('thickbox.css', '/' . WPINC . '/js/thickbox/thickbox.css', null, '1.0');
-        wp_enqueue_script('ajax-menu-loader-js', get_stylesheet_directory_uri(). '/js/ajax-menu-loader.js', array(), '20120206', true);
-	    if(is_single('kazua-das-artes')){
+	    //if(is_single('kazua-das-artes')){
+		    wp_enqueue_style('jetpack-carousel', home_url(). '/wp-content/plugins/jetpack/modules/carousel/jetpack-carousel.css', null, '1.0');
+		    //wp_enqueue_style('jetpack-carousel-css', home_url(). '/wp-content/plugins/jetpack/modules/carousel/jetpack-carousel.css', array(), '20120206', true);
 
+		    wp_enqueue_script('ajax-menu-loader-js', get_stylesheet_directory_uri(). '/js/ajax-menu-loader.js', array(), '20120206', true);
+		    wp_enqueue_script('jetpack-carousel', home_url(). '/wp-content/plugins/jetpack/modules/carousel/jetpack-carousel.js', array(), '20120206', true);
+		    // Note: using  home_url() instead of admin_url() for ajaxurl to be sure  to get same domain on wpcom when using mapped domains (also works on self-hosted)
+		    // Also: not hardcoding path since there is no guarantee site is running on site root in self-hosted context.
+		    // $is_logged_in = is_user_logged_in();
+		    $current_user = wp_get_current_user();
+		    $comment_registration = intval( get_option( 'comment_registration' ) );
+		    $require_name_email   = intval( get_option( 'require_name_email' ) );
+		    $is_logged_in = is_user_logged_in();
+		    $localize_strings = array(
+			    'widths'               => 370,700,1000,1200,1400,2000,
+			    'is_logged_in'         => $is_logged_in,
+			    'lang'                 => strtolower( substr( get_locale(), 0, 2 ) ),
+			    'ajaxurl'              => set_url_scheme( admin_url( 'admin-ajax.php' ) ),
+			    'nonce'                => wp_create_nonce( 'carousel_nonce' ),
+			    //  'display_exif'         => $this->test_1or0_option( get_option( 'carousel_display_exif' ), true ),
+			    //  'display_geo'          => $this->test_1or0_option( get_option( 'carousel_display_geo' ), true ),
+			    'background_color'     => 'black',
+			    'comment'              => __( 'Comment', 'jetpack' ),
+			    'post_comment'         => __( 'Post Comment', 'jetpack' ),
+			    'write_comment'        => __( 'Write a Comment...', 'jetpack' ),
+			    'loading_comments'     => __( 'Loading Comments...', 'jetpack' ),
+			    'download_original'    => sprintf( __( 'View full size <span class="photo-size">%1$s<span class="photo-size-times">&times;</span>%2$s</span>', 'jetpack' ), '{0}', '{1}' ),
+			    'no_comment_text'      => __( 'Please be sure to submit some text with your comment.', 'jetpack' ),
+			    'no_comment_email'     => __( 'Please provide an email address to comment.', 'jetpack' ),
+			    'no_comment_author'    => __( 'Please provide your name to comment.', 'jetpack' ),
+			    'comment_post_error'   => __( 'Sorry, but there was an error posting your comment. Please try again later.', 'jetpack' ),
+			    'comment_approved'     => __( 'Your comment was approved.', 'jetpack' ),
+			    'comment_unapproved'   => __( 'Your comment is in moderation.', 'jetpack' ),
+			    'camera'               => __( 'Camera', 'jetpack' ),
+			    'aperture'             => __( 'Aperture', 'jetpack' ),
+			    'shutter_speed'        => __( 'Shutter Speed', 'jetpack' ),
+			    'focal_length'         => __( 'Focal Length', 'jetpack' ),
+			    // 'comment_registration' => $comment_registration,
+			    // 'require_name_email'   => $require_name_email,
+			    'login_url'            => wp_login_url( apply_filters( 'the_permalink', get_permalink() ) ),
+		    );
+
+		    if ( ! isset( $localize_strings['jetpack_comments_iframe_src'] ) || empty( $localize_strings['jetpack_comments_iframe_src'] ) ) {
+			    // We're not using Jetpack comments after all, so fallback to standard local comments.
+
+			    if ( $is_logged_in ) {
+				    $localize_strings['local_comments_commenting_as'] = '<p id="jp-carousel-commenting-as">' . sprintf( __( 'Commenting as %s', 'jetpack' ), $current_user->data->display_name ) . '</p>';
+			    } else {
+				    if ( $comment_registration ) {
+					    $localize_strings['local_comments_commenting_as'] = '<p id="jp-carousel-commenting-as">' . __( 'You must be <a href="#" class="jp-carousel-comment-login">logged in</a> to post a comment.', 'jetpack' ) . '</p>';
+				    } else {
+					    $required = ( $require_name_email ) ? __( '%s (Required)', 'jetpack' ) : '%s';
+					    $localize_strings['local_comments_commenting_as'] = ''
+					                                                        . '<fieldset><label for="email">' . sprintf( $required, __( 'Email', 'jetpack' ) ) . '</label> '
+					                                                        . '<input type="text" name="email" class="jp-carousel-comment-form-field jp-carousel-comment-form-text-field" id="jp-carousel-comment-form-email-field" /></fieldset>'
+					                                                        . '<fieldset><label for="author">' . sprintf( $required, __( 'Name', 'jetpack' ) ) . '</label> '
+					                                                        . '<input type="text" name="author" class="jp-carousel-comment-form-field jp-carousel-comment-form-text-field" id="jp-carousel-comment-form-author-field" /></fieldset>'
+					                                                        . '<fieldset><label for="url">' . __( 'Website', 'jetpack' ) . '</label> '
+					                                                        . '<input type="text" name="url" class="jp-carousel-comment-form-field jp-carousel-comment-form-text-field" id="jp-carousel-comment-form-url-field" /></fieldset>';
+				    }
+			    }
+		    }
+		    $localize_strings = apply_filters( 'jp_carousel_localize_strings', $localize_strings );
+		    wp_localize_script( 'jetpack-carousel', 'jetpackCarouselStrings', $localize_strings );
 	    }
-    }
+    //}
 }
 
-add_action('init', 'add_themescript');
+add_action('wp_enqueue_scripts', 'add_themescript',9999999999);
 
 
 /*
