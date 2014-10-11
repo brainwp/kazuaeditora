70d41866a3b7e802d32cb51baeaecae37cafc4f9
Your Name <diego@brasa.art.br>
2014-10-10 23:33:02 -0300

    refeito o loop da single.php para chamar só um post

--- wp-content/themes/tema-kazua/single.php
+++ wp-content/themes/tema-kazua/single.php
@@ -12,9 +12,44 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" class="fundo-blog" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+				 <!-- Inicio Loop -->
+		<?php $i = 1; while (have_posts() && $i < 2) : the_post(); ?>
+		
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		
+			<header class="entry-header">
 
-				<?php get_template_part( 'content', get_post_format() ); ?>
+		      <h1 class="entry-title"><?php the_title(); ?></h1>
+
+				<div class="entry-data-single">
+					<?php the_time('d/m/Y'); ?>
+				</div><!-- .entry-data -->
+
+				<nav class="nav-single">
+					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
+					<span class="nav-previous"><?php previous_post_link( '%link', '%title <span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span>' ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+				</nav><!-- .nav-single -->
+
+		 <?php if ( has_post_thumbnail() ) { ?>
+                    <?php the_post_thumbnail(); ?>
+                <?php } ?>	
+			</header><!-- .entry-header -->
+
+		<div class="entry-content">
+
+			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+
+		</div><!-- .entry-content -->
+
+			<nav class="nav-single">
+					<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
+					<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
+					<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
+			</nav><!-- .nav-single -->		
+
+		</article><!-- #post -->
 
 					<?php if ( have_comments() ) : ?>
 					<?php comments_template( '', true ); ?>
@@ -24,7 +59,11 @@ get_header(); ?>
 					<?php endif; // end ! comments_open() ?>
 				<?php endif; // end have_comments() ?>
 
-			<?php endwhile; // end of the loop. ?>
+			<?php $i++; endwhile; // end of the loop. ?>
+
+			<?php wp_reset_query(); // reset query ?>
+
+				    <!-- Fim Loop -->
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
